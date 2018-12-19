e2bd3e62b8421d5850769359df3ea07ccbb0a8b8
Maksym Kovalskyi <maxkow235@gmail.com>
2018-12-19 19:59:53 +0200

    Updates

---
+++ precache-manifest.008d2fff49a6afd87949a67869f6a77f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da8d1ebc484a1e6c4a8",
+    "url": "/react-step-form/static/js/runtime~main.4da8d1eb.js"
+  },
+  {
+    "revision": "1af5b0165a99cb8ad03e",
+    "url": "/react-step-form/static/js/main.1af5b016.chunk.js"
+  },
+  {
+    "revision": "fce8ced0fe458b6ec14e",
+    "url": "/react-step-form/static/js/1.fce8ced0.chunk.js"
+  },
+  {
+    "revision": "1af5b0165a99cb8ad03e",
+    "url": "/react-step-form/static/css/main.dda05391.chunk.css"
+  },
+  {
+    "revision": "7a4bc833b4ac437078aaec94e865a5ac",
+    "url": "/react-step-form/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1080b5662f2af6ad77fa0f7276a331b8.js"
+  "/react-step-form/precache-manifest.008d2fff49a6afd87949a67869f6a77f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-step-form/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
