15bde29180de14f56fe47cbcfff9ee3326244273
Maksym Kovalskyi <maxkow235@gmail.com>
2018-12-19 19:55:01 +0200

    Updates

---
+++ precache-manifest.1080b5662f2af6ad77fa0f7276a331b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10adb566365e8ae7ff1e",
+    "url": "/static/js/main.10adb566.chunk.js"
+  },
+  {
+    "revision": "fce8ced0fe458b6ec14e",
+    "url": "/static/js/1.fce8ced0.chunk.js"
+  },
+  {
+    "revision": "10adb566365e8ae7ff1e",
+    "url": "/static/css/main.dda05391.chunk.css"
+  },
+  {
+    "revision": "2a394b605b25ab1f29c3c9c3edefbd1c",
+    "url": "/index.html"
+  }
+];
